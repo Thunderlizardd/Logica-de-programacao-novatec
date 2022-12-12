8c0911a198556dff67a47a59a16f78902ef0be72
Deivison Cardoso <deivisonjr@hotmail.com>
2022-12-12 18:26:03 -0300

    Concluindo os cálculos

---
+++ Student/script.js
@@ -0,0 +1,22 @@
+
+function schoolAverage () {
+
+    // criar referencia aos elementos da página
+    var inStudentName = document.getElementById("inStudentName");
+    var inResultExameOne = document.getElementById("inResultExameOne");
+    var inResultExameTwo = document.getElementById("inResultExameTwo");
+    var result1 = document.getElementById("result");
+    var result2 = document.getElementById("result2");
+      
+}
+
+// obter o conteúdo dos campos de entrada
+
+var nameStudent = (inStudentName.value);
+var exameOne = (inResultExameOne.value);
+var exameTwo = (inResultExameTwo.value);
+
+// calcular os valores
+
+
+
