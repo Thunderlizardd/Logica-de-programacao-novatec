e57a494a04081475b6f63277783bf8afeeaf6d13
Deivison Cardoso <deivisonjr@hotmail.com>
2023-03-27 13:57:15 -0300

    Correção

--- Programa parquímetro/script.js
+++ Programa parquímetro/script.js
@@ -2,26 +2,32 @@ function parquimetro() {
 
     // fazer referência aos elementos da página
     var inValor = document.getElementById("inValor");
-    var outResult = document.getElementById("outResult");
+    var outResult1 = document.getElementById("outResult1");
+    var outResult2 = document.getElementById("outResult2");
 
     // capturar e transformar os valores dos campos
     var valor = Number(inValor.value);
 
     // verificar campos 
 
+  
 
 
-    if (valor == "" || valor == 0 || valor(isNaN)) {
-        alert("Informe um valor válido"); // exibe uma alerta
-        inValor.focus();
-        return;
-    }
+    tempo = valor * 5
 
 
 
-    outResult.textContent = valor
+    outResult1.textContent = "Tempo: " + tempo;
+    outResult1.textContent = "Tempo: " + tempo;
+
+
 }
 
+// criar referência ao elemento btaPagar
+
 var btConfirmar = document.getElementById("btConfirmar");
+
+// registra um evento associado ao botão para carregar um função
+
 btConfirmar.addEventListener("click", parquimetro);
 
