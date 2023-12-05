162520d9e7eb06538f1d950fd365bf9ab625529a
Thunderlizardd <deivisonjr@hotmail.com>
2023-12-05 19:36:58 -0300

    teste

---
+++ Programa Lados de um Triângulo/script.js
@@ -0,0 +1,25 @@
+
+function sidesOfaTriangle() {
+
+    // create reference to the elements of the page
+
+    var inSideA = document.getElementById("inSideA");
+    var inSideB = document.getElementById("inSideB");
+    var inSideC = document.getElementById("inSideC");
+
+    // get content of the entrance filds 
+
+    var sideA = (inSideA);
+    var sideB = (inSideB);
+    var sideC = (inSideC);
+    
+
+
+
+
+
+
+
+
+
+} 
